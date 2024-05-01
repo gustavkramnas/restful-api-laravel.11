12e0791d12b98e900e939ecc0b6726ff0f0a84ab
Gustav Kramnäs <gustav@kramnas.se>
2024-05-01 14:05:39 +0200

    Added migrations: absence_reports table schema

---
+++ database/migrations/2024_04_29_115233_create_absence_report_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    public function up()
+    {
+        Schema::create('absence_reports', function (Blueprint $table) {
+            $table->id();
+            $table->string('employee_id');
+            $table->date('date');
+            $table->string('reason');
+            $table->string('absence_type');
+            $table->boolean('medical_certificate');
+            $table->json('medical_certificate_photos');
+            $table->string('approval_by');
+            $table->date('approval_date');
+            $table->string('comments');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('absence_reports');
+    }
+};
