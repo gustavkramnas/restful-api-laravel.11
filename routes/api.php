5170b6fdbf6a69db0165f0c67d959ba1ed383494
Gustav Kramnäs <gustav@kramnas.se>
2024-05-08 14:03:14 +0200

    Adding route to display absence creation form at root "create_absence_blade"-file & set name for the absence post route.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
 use App\Http\Controllers\AbsenceController;
+use Illuminate\Support\Facades\Route;
 
-Route::post('/absences', [AbsenceController::class, 'store']);
+Route::post('/absences', [AbsenceController::class, 'store'])->name('absences');
+
+Route::get('/', [AbsenceController::class, 'create_absence_blade']);
+
