11c0c09416dee4932bd30aa8f29464ccfc34fa02
Gustav Kramnäs <gustav@kramnas.se>
2024-05-14 13:44:17 +0200

    Re-factor: - deleting                 'absence_certificate_photos' => 'array', - deleting data validation connected to that attribut

--- app/Http/Controllers/AbsenceController.php
+++ app/Http/Controllers/AbsenceController.php
@@ -24,10 +24,8 @@ class AbsenceController extends Controller
     public function store(Request $request)
     {
         try {
-
             // Validera och filtrera värdet av 'absence_certificate'-parametern för att se om det är sant eller falskt.
             $request->merge(['absence_certificate' => filter_var($request->input('absence_certificate'), FILTER_VALIDATE_BOOLEAN)]);
-
 
             $validatedData = $request->validate([
                 'employee_id' => 'required',
@@ -38,22 +36,10 @@ class AbsenceController extends Controller
                 'absence_type' => 'required',
                 'absence_percentage_level' => 'required',
                 'absence_certificate' => 'boolean',
-                'absence_certificate_photos' => 'array',
                 'approval_by' => 'required',
                 'approval_date' => 'required|date',
                 'comments' => 'required',
             ]);
-
-            // Check if absence_certificate_photos array is empty
-            if (empty($validatedData['absence_certificate_photos'])) {
-                $validatedData['absence_certificate_photos'] = ['user did not upload any file'];
-            }
-
-            // Check for any extra attributes
-            $extraAttributes = array_diff_key($request->all(), array_flip(array_keys($validatedData)));
-            if (!empty($extraAttributes)) {
-                throw ValidationException::withMessages(['extra_attributes' => 'Extra attribut är inte tillåtna.']);
-            }
 
             // Skapa frånvaron med de validerade uppgifterna och lagra den i databasen.
             $absence = AbsenceModel::create($validatedData);
