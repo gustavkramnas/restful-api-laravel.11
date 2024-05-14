92c0eac6daad6a9bf963444795d94ff1752ada4f
Gustav Kramnäs <gustav@kramnas.se>
2024-05-14 13:46:05 +0200

    Re-factor: - deleting attribute & cast property

--- app/Models/AbsenceModel.php
+++ app/Models/AbsenceModel.php
@@ -18,14 +18,9 @@ class AbsenceModel extends Model
         'absence_type',
         'absence_percentage_level',
         'absence_certificate',
-        'absence_certificate_photos',
         'approval_by',
         'approval_date',
         'comments',
-    ];
-
-    protected $casts = [
-        'absence_certificate_photos' => 'array',
     ];
 
 }
