de178088e10b89a72031d5da61cd3503564fb5d2
Gustav Kramnäs <gustav@kramnas.se>
2024-05-15 23:27:31 +0200

    adding comment lines: -Api routes -Web routes

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\AbsenceController;
 use Illuminate\Support\Facades\Route;
 
+// Api routes
 Route::post('/absences', [AbsenceController::class, 'store'])->name('absences');
 
-Route::get('/', [AbsenceController::class, 'create_absence_blade']);
 
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AbsenceController;
 
-
+// Web routes
 Route::get('/', [AbsenceController::class, 'submitAbsenceReport']);
 
 Route::get('/submittedAbsenceReport/{id}', [AbsenceController::class, 'showSubmittedAbsenceReport'])->name('showSubmittedAbsenceReport');
